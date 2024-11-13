7d58b4d66922afcb205151fd5d97dcf4509ba356
Tanay Mantri <112106166+TanayFer@users.noreply.github.com>
2024-11-13 21:20:44 +0530

    Update MenuDrivenArithmaticFunc.c

--- C Codes/Menu Driven Arithmatic Operators/MenuDrivenArithmaticFunc.c
+++ C Codes/Menu Driven Arithmatic Operators/MenuDrivenArithmaticFunc.c
@@ -2,16 +2,19 @@
 #include<conio.h>
 int addNum(int,int); 
 int subNum(int,int);
+int multNum(int,int);
 float mulNum(float,float);
+int diviNum(int ,int);
 float divNum(float,float);
 int sqNo(long);       
 int cubeNo(long);  
-long fact(long);                 //Function Prototype
+long fact(long);      
+int power(int , int);            //Function Prototype
 void main()                      //Function main
 {
 	int result1 , result2, result3;
 	int a, b; 
-	int result5, result6, result7; 
+	int result5, result6, result7, result8, result9, result10; 
 	float result4;
 	char OPT , loop;
 	do
@@ -20,10 +23,14 @@ void main()                      //Function main
 	printf("1 :- Addition\n");
 	printf("2 :- Substraction\n");
 	printf("3 :- Multiplication\n");
-	printf("4 :- Division\n");
-	printf("5 :- Square of a Number\n");
-	printf("6 :- Cube of a Number\n");
-	printf("7 :- Factorial of a Number\n");
+	printf("4 :- Multiplication in Decimal Value\n");
+	printf("5 :- Division\n");
+	printf("6 :- Division in Decimal Value\n");
+	printf("7 :- Square of a Number\n");
+	printf("8 :- Cube of a Number\n");
+	printf("9 :- Factorial of a Number\n");
+	printf("10 :- Power\n");
+
 	printf("------------------------------------------------\n");
 	scanf("%d", &OPT);
 	printf("------------------------------------------------\n"); 
@@ -35,19 +42,28 @@ void main()                      //Function main
 		case 2 :  result2 = subNum(a, b);
 		break;
 		
-		case 3 :  result3 = mulNum(a, b);
+		case 3 :  result3 = multNum(a, b);
+		break;
+
+		case 4 :  result4 = mulNum(a, b);
+		break;
+
+		case 5 :  result5 = diviNum(a, b);
 		break;
 		
-		case 4 :  result4 = divNum(a, b);
+		case 6 :  result6 = divNum(a, b);
 		break;
 		
-		case 5 :  result5 = sqNo(a);
+		case 7 :  result7 = sqNo(a);
 		break;  
 		
-		case 6 :  result6 = cubeNo(a);
+		case 8 :  result8 = cubeNo(a);
 		break;
 		
-		case 7 :  result7 = fact(a);
+		case 9 :  result9 = fact(a);
+		break;
+
+		case 10 :  result10 = power(a,b);
 		break;
     }
 	printf("-----------------------\nMain Menu : Enter 1\n");
@@ -77,6 +93,16 @@ int subNum(int x, int y)       //Function Definition
 	return 0;
 }
 
+int multNum(int x, int y)       //Function Definition
+{
+	int e;
+	printf("Enter 2 numbers\n");
+	scanf("%d %d", &x , &y);
+	e = x * y;
+	printf("Multiplication of %d * %d = %d\n", x,y,e);
+	return 0;
+}
+
 float mulNum(float x, float y)       //Function Definition
 {
 	float e;
@@ -84,6 +110,16 @@ float mulNum(float x, float y)       //Function Definition
 	scanf("%f %f", &x , &y);
 	e = x * y;
 	printf("Multiplication of %.2f * %.2f = %.4f\n", x,y,e);
+	return 0;
+}
+
+int diviNum(int x, int y)       //Function Definition
+{
+	int f;
+	printf("Enter 2 numbers\n");
+	scanf("%d %d", &x , &y);
+	f = x / y;
+	printf("Division of %d / %d = %d\n", x,y,f);
 	return 0;
 }
 
@@ -127,4 +163,19 @@ long fact(long x)
     }
     printf("Factorial of %d: %d\n", x, factorial);
     return factorial; 
+}
+
+int power(int a, int b)
+{
+	int p=1;
+	printf("Enter The First Number\n");
+	scanf("%d",&a);
+	printf("Enter The Power\n");
+	scanf("%d",&b);
+	for(int i=1; i<=b; i++)
+	{
+		p *= a;
+	}
+	printf("Power Is %d\n", p);
+	return 0;
 }
