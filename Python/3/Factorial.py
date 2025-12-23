bdd66238bd760ee1b43f3d31b1ac48495ab5298f
Tanay Mantri <tanaymantri28@gmail.com>
2025-12-23 09:01:05 +0530

    Add Python scripts for Factorial, Fibonacci, Floyd
    Added Factorial.py and Fibonacci.py implementing basic algorithms for factorial and Fibonacci sequence. Created Floyd.py as a new file (currently empty). Updated Java/Testing.class binary.

---
+++ Python/3/Factorial.py
@@ -0,0 +1,6 @@
+A = int(input('Enter Number : '));
+C = 1;
+for i in range(1, A+1):
+    C = C * i;
+    
+print(C);
---
+++ Python/3/Fibonacci.py
@@ -0,0 +1,5 @@
+A = int(input('Enter a number: '))
+a, b = 0, 1
+for i in range(A):
+    print(a)
+    a, b = b, a + b
